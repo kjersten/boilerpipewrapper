39652c6bdd24eabf864e8027e78189713682c2ef
Kjersten Elias <kjersten.elias@gmail.com>
2012-10-06 20:59:18 +0700

    stopping myself before I use reflection to implement parser selection

--- src/boilerpipewrapper/core.clj
+++ src/boilerpipewrapper/core.clj
@@ -7,10 +7,22 @@
     java.io.InputStream
     org.xml.sax.InputSource))
 
+(def parser-options '("ArticleExtractor" "DefaultExtractor" "LargestContentExtractor"
+                      "KeepEverythingExtractor" "CanolaExtractor"))
+
 (defn -main
   "Accepts a URL, parses the main content from that page and prints it to STDOUT"
   [& args]
-  (if (or (nil? args) (not (.startsWith (first args) "http://"))) (println "Please provide a URL.")
-   (println (.getText ArticleExtractor/INSTANCE 
-     (new InputSource (.openStream (new URL (first args)))))))
+  (let [url (first args) 
+        extractor-arg (second args)
+        extractor (if (and extractor-arg (some #{extractor-arg} parser-options)) extractor-arg "ArticleExtractor")] 
+	(println parser-options)
+    (println (if (nil? extractor) "here is where the extractor string would go" extractor))
+    (if (not (.startsWith url "http://")) 
+      (println "Please provide a URL.")
+      (println (class extractor))
+      ;; (println (doto (extractor .INSTANCE) (.getText (new InputSource (.openStream (new URL url))))))
+       ;;(.getText extractor/INSTANCE (new InputSource (.openStream (new URL url))))))
+	)
+   )
   )
