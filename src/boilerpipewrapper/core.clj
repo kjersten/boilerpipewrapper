3a57aa79b74998334984a126a9c51291b70c0894
Kjersten Elias <kjersten.elias@gmail.com>
2012-10-06 18:17:10 +0700

    converting the URL content into a InputStream/Source before submitting it for parsing, as recommended

--- src/boilerpipewrapper/core.clj
+++ src/boilerpipewrapper/core.clj
@@ -3,11 +3,14 @@
      :name net.bitzoo.BoilerpipeWrapper)
   (:import 
    	de.l3s.boilerpipe.extractors.ArticleExtractor
-  	java.net.URL))
+  	java.net.URL
+    java.io.InputStream
+    org.xml.sax.InputSource))
 
 (defn -main
-  "Just print a string"
+  "Accepts a URL, parses the main content from that page and prints it to STDOUT"
   [& args]
   (if (or (nil? args) (not (.startsWith (first args) "http://"))) (println "Please provide a URL.")
-   (println (.getText ArticleExtractor/INSTANCE (new URL (first args)))))
+   (println (.getText ArticleExtractor/INSTANCE 
+     (new InputSource (.openStream (new URL (first args)))))))
   )
